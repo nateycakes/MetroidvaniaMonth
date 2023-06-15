4a6462f5907a9e488f88e7971835e35112f0f435
natey <natey@nateycakes.com>
2023-06-15 00:15:54 -0400

    More UI Framework, Added Total Collectibles at the End Screen

--- src/ui/Win Screen.gd
+++ src/ui/Win Screen.gd
@@ -1,5 +1,14 @@
 extends Control
 
+onready var secretCountLabel : Label = $MarginContainer/VBoxContainer/SecretCountLabel
+
 func _ready() -> void:
 	ScreenTransitions.play_transition("DEFAULT_IN")
 	MusicPlayer.crossfade_songs(MusicPlayer.library.VICTORY_SCREEN)
+	set_secret_count_label()
+
+
+
+func set_secret_count_label():
+	var secretCount : int = Events.lorebook_entries.size()
+	secretCountLabel.text = "You collected: " + str(secretCount) + " / 19 Secrets"
